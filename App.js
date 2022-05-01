2950be0ff4c2c5788000ac0fb8f2bc06478faeab
Ragib Sakib <ragibsakib2002@gmail.com>
2022-05-01 15:02:44 -0700

    3 pages test
    Test of Navigation between three pages via buttons.

--- App.js
+++ App.js
@@ -1,12 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import {StyleSheet, Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function TempHomeScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Take This Quiz for the App</Text>
+      <Button
+        title="Get Started"
+        onPress={() => navigation.navigate('Quiz')}
+      />
     </View>
+  );
+}
+
+function QuizScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.top}>
+      <Text>You Like Games?</Text>
+
+      <View style={styles.bottom}>
+        <Button
+          title='Nah Not really'
+          onPress={() => navigation.navigate('RealHome')}
+          />
+      </View>
+      </View>
+    </View>
+  );
+}
+
+function RealHome(navigation) {
+  return (
+    <View style = {styles.container}>
+      <Text>Oh....</Text>
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="TempHome" 
+          component={TempHomeScreen}  
+          options={{
+            headerShown: false
+          }}
+          />
+        <Stack.Screen 
+          name="Quiz" 
+          component={QuizScreen} 
+          />
+        <Stack.Screen
+          name="RealHome"
+          component={RealHome}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +76,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  top: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 100
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 36
+  }
 });
+
+export default App;
