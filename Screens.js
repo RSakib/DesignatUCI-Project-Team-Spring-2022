8e62f46622dedf322244fde62b705dee4102bf65
Ragib Sakib <ragibsakib2002@gmail.com>
2022-05-08 20:02:50 -0700

    Added Scrolling
    But the Big button is broken

--- Screens.js
+++ Screens.js
@@ -1,8 +1,8 @@
-import {StyleSheet, Button, View, Text } from 'react-native';
+import {StyleSheet, Button, View, Text, FlatList, SafeAreaView } from 'react-native';
 
 export function StartScreen({navigation}) {
 return (
-    <View style={styles.container}>
+    <View style={My_styles.container}>
         <Text>Take This Quiz for the App</Text>
         <Button
             title="Get Started"
@@ -14,15 +14,15 @@ return (
 
 export function QuizScreen({navigation}) {
 return (
-    <View style={styles.container}>
+    <View style={My_styles.container}>
 
-        <View style={styles.top}>
+        <View style={My_styles.top}>
         <Text>You Like Games?</Text>
 
-            <View style={styles.bottom}>
+            <View style={My_styles.bottom}>
                 <Button
                 title='Nah Not really'
-                onPress={() => navigation.navigate('homeScreen')}
+                onPress={() => navigation.navigate('HomeScreen')}
                 />
             </View>
         </View>
@@ -32,19 +32,16 @@ return (
 
 export function HomeScreen(navigation) {
 return (
-    <View style={[styles.container, {flexDirection: "column"}]}>
-        <View style={{ flex: 1, backgroundColor: "red" , flexBasis:280}} />
-        <View style={{ flex: 1, backgroundColor: "darkorange", flexBasis:210 }} />
-        <View style={{ flex: 1, backgroundColor: "green", flexBasis:210 }} />
-        <View style={{ flex: 1, backgroundColor: "darkorange", flexBasis:210 }} />
-      </View>
+    <SafeAreaView style={[My_styles.container, {flexDirection: "column"}]}>
+        <List/>
+    </SafeAreaView>
 )
 }
 
 export function SearchScreen(navigation) {
     return (
-        <View style={[styles.container, {flexDirection: "column"}]}>
-            <View style={[styles.center]}>
+        <View style={[My_styles.container, {flexDirection: "column"}]}>
+            <View style={[My_styles.center]}>
                 <Text>Searching for some sanity here</Text>
             </View>
         </View>
@@ -53,8 +50,8 @@ export function SearchScreen(navigation) {
 
 export function BrowseScreen(navigation) {
     return (
-        <View style={[styles.container, {flexDirection: "column"}]}>
-            <View style={[styles.center]}>
+        <View style={[My_styles.container, {flexDirection: "column"}]}>
+            <View style={[My_styles.center]}>
                 <Text>Browsing for some sanity, idk man its a Sunday</Text>
             </View>
         </View>
@@ -62,7 +59,7 @@ export function BrowseScreen(navigation) {
 }
 
 
-const styles = StyleSheet.create({
+const My_styles = StyleSheet.create({
     container: {
       flex: 1,
     },
@@ -81,5 +78,83 @@ const styles = StyleSheet.create({
         flex:1,
         alignItems: "center",
         justifyContent: 'center',
+    }
+  });
+
+  const data = [
+    {id:0, text: 'Empty'},
+    {id:1, text: 'One'},
+    {id:2, text: 'Cool'},
+    {id:3, text: 'lit'},
+    {id:4, text: 'One'},
+    {id:5, text: 'Cool'},
+    {id:6, text: 'lit'},
+    {id:7, text: 'One'},
+    {id:8, text: 'Cool'},
+    {id:9, text: 'lit'},
+    {id:10, text: 'One'},
+    {id:11, text: 'Cool'},
+    {id:12, text: 'lit'}
+  ]
+
+  function Slide_Deck(navigation) {
+      return(
+        <View style={styles.red_item}>
+        <Button
+        title='Nah Not really'
+        onPress={() => navigation.navigate('homeScreen')}
+        />
+        </View>
+      )
+    
+  }
+  
+  const Item = ({item}) => {
+      if (item.id == 0) {
+        return (
+            <Slide_Deck/>
+          )
+      }
+      else {
+        return (
+            <View style={[styles.item, styles.shadow]}>
+              <Text>{item.text}</Text>
+            </View>
+          );
+      }
+    
+  }
+  
+  let List = () => {
+    return (
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={data}
+        renderItem={Item}
+        keyExtractor={item => item.id}
+      />
+    );
+  }
+
+  const styles = StyleSheet.create({
+    item: {
+      backgroundColor: '#fff',
+      padding: 20,
+      marginHorizontal: 10,
+      marginBottom: 3.5,
+      flexDirection: 'row'
+    },
+    red_item: {
+        backgroundColor: 'red',
+        padding: 150
+      },
+    shadow: {
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 10
+      },
+      shadowOpacity: 0.47,
+      shadowRadius: 9,
     }
   });
